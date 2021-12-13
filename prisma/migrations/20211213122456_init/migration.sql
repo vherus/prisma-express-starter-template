95d49e583f07fcb7a477209648d5833ecc3d31bf
Nathan King <nathanking3590@gmail.com>
2021-12-13 12:35:06 +0000

    Ready for production!

---
+++ prisma/migrations/20211213122456_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
